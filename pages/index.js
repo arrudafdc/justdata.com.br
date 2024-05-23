d4e9f80e6611ddc736c85aa7617883d45e672b4e
arrudafdc <arrudafdc@gmail.com>
2024-05-23 17:12:38 -0300

    change stack for react

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h1>Just Data!</h1>;
-}
-
-export default Home;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Just Data!</h1>
+    </>
+  );
+}
+
+export default App;
